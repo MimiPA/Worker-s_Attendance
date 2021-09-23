ab0e79dfe03ba417c9dc7a8259c53d82e604aede
MimiPA <paramitaaditung@yahoo.com>
2021-09-23 22:57:38 +0800

    Endpoint GET login & register

--- route/index.js
+++ route/index.js
@@ -15,6 +15,23 @@ router.get('/', (req, res) => {
     res.status(200).send("Glints Worker's Attendance");
 });
 
+router.get('/login', (req, res) => {
+    res.status(200).send({ status: 'success', message: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9yZWdpc3RlciI6MiwiZW1haWwiOiJwYXJhbWl0YWFkaXR1bmdAZ21haWwuY29tIiwiaWF0IjoxNjMyNDAxOTkzLCJleHAiOjE2MzI0MDU1OTN9.0CSMu2GElkyIILuuThRNQGFgGsL4vLL10OJFiBpmcaY' });
+});
+
+router.get('/register', (req, res) => {
+    res.status(201).json({
+        status: 'success',
+        message: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcmFtaXRhYWRpdHVuZ0BnbWFpbC5jb20iLCJpYXQiOjE2MzIxNDc2MTgsImV4cCI6MTYzMjE1MTIxOH0.ZB2cVWxHP3TJvE5xn_9nZ-WEzT_HTmJUX4W0PZu81os',
+        data: {
+            id_register: 1,
+            full_name: "Natasya Susanto",
+            email: "natasya.susanto01@gmail.com",
+            password: "$2b$10$Eq2hk7cvajiyQNOQuCP1vezcP.0fdvVfI67M44eHhUmyEB7Tv5QCy"
+        }
+    });
+});
+
 router.post('/register', registerHandler);
 router.post('/login', loginHandler);
 
