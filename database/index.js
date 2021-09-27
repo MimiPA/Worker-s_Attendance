b94452c7c55b9913f96f455dafb8129b2189610f
MimiPA <paramitaaditung@yahoo.com>
2021-09-27 22:01:01 +0800

    database

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = {};
 const sequelize = new Sequelize('denndsuq8po91s', 'uemfoqlmslqxla', 'feaa718624322e4d96fbf7f2b4575e814ead63f7a8860c58385a2dd278536f03', {
-    host: 'localhost',
+    host: 'ec2-52-206-193-199.compute-1.amazonaws.com',
     dialect: 'postgres',
     operatorsAliases: false,
     port: 5432,
