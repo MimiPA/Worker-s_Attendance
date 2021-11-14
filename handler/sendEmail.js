ca7e399d6c2ef7ce90747744a1d8116cbeb8c08c
Paramita <paramitaaditung@yahoo.com>
2021-11-14 22:34:35 +0800

    change

--- handler/sendEmail.js
+++ handler/sendEmail.js
@@ -2,17 +2,17 @@ const nodemailer = require("nodemailer");
 const { google } = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 
-const oauth2Client = new OAuth2(
-    "593027374094-d9sd3sht7nsfadgvlaoda4s2nljur06q.apps.googleusercontent.com",
-    "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",
-    "https://developers.google.com/oauthplayground"
-);
+// const oauth2Client = new OAuth2(
+//     "593027374094-d9sd3sht7nsfadgvlaoda4s2nljur06q.apps.googleusercontent.com",
+//     "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",
+//     "https://developers.google.com/oauthplayground"
+// );
 
-oauth2Client.setCredentials({
-    refresh_token: "1//04LRwQfJVksgZCgYIARAAGAQSNwF-L9IrgAMgbjKxILzhN7NnCCM39wRTNrw8dplC0x7dTGoG8SZ6bP2KDepLEEsanTzKI4qTHiU"
-});
+// oauth2Client.setCredentials({
+//     refresh_token: "1//04LRwQfJVksgZCgYIARAAGAQSNwF-L9IrgAMgbjKxILzhN7NnCCM39wRTNrw8dplC0x7dTGoG8SZ6bP2KDepLEEsanTzKI4qTHiU"
+// });
 
-const accessToken = oauth2Client.getAccessToken();
+//const accessToken = oauth2Client.getAccessToken();
 
 const sendEmail = async (email, subject, text) => {
     try {
@@ -28,7 +28,7 @@ const sendEmail = async (email, subject, text) => {
                 clientId: "593027374094-d9sd3sht7nsfadgvlaoda4s2nljur06q.apps.googleusercontent.com",
                 clientSecret: "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",
                 refreshToken: "1//04LRwQfJVksgZCgYIARAAGAQSNwF-L9IrgAMgbjKxILzhN7NnCCM39wRTNrw8dplC0x7dTGoG8SZ6bP2KDepLEEsanTzKI4qTHiU",
-                accessToken: accessToken,
+                //accessToken: accessToken,
                 //pass: 'IpeBe1309'
             },
             tls: {
--- handler/usersHandler.js
+++ handler/usersHandler.js
@@ -6,52 +6,52 @@ userModel.hasMany(attendanceModel, {foreignKey: 'id_register'});
 
 const usersHandler = async (req, res) => {
     try {
-        // const token = req.body.token || req.query.token || req.headers["x-access-token"];
+        const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
-        // if (!token) {
-        //     return res.status(403).send("A token is required for authentication");
-        // }
-        // else {
-        //     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        if (!token) {
+            return res.status(403).send("A token is required for authentication");
+        }
+        else {
+            const decoded = jwt.verify(token, process.env.TOKEN_KEY);
 
-        //     if (decoded.id_level == 1) {
-        //         return res.status(400).send({ status: 'failed', message: 'This is not admin' });
-        //     }
-        //     else if (decoded.id_level == 2) {
-        //         const user = await userModel.findAll({
-        //             attributes: { exclude: ['password', 'token'] },
-        //             order: [['id_register', 'DESC']]
-        //         });
-
-        //         // const check = await userModel.findAll({
-        //         //     include: [{
-        //         //         model: attendanceModel,
-        //         //         required: true
-        //         //     }]
-        //         // });
-
-        //         return res.status(200).send({
-        //             status: 'success',
-        //             data: {
-        //                 user
-        //             }
-        //         });
-        //     }
-        // }
-
-
-
-        const user = await userModel.findAll({
-            attributes: { exclude: ['password', 'token'] },
-            order: [['id_register', 'DESC']]
-        });
-
-        return res.status(200).send({
-            status: 'success',
-            data: {
-                user
+            if (decoded.id_level == 1) {
+                return res.status(400).send({ status: 'failed', message: 'This is not admin' });
             }
-        });
+            else if (decoded.id_level == 2) {
+                const user = await userModel.findAll({
+                    attributes: { exclude: ['password', 'token'] },
+                    order: [['id_register', 'DESC']]
+                });
+
+                // const check = await userModel.findAll({
+                //     include: [{
+                //         model: attendanceModel,
+                //         required: true
+                //     }]
+                // });
+
+                return res.status(200).send({
+                    status: 'success',
+                    data: {
+                        user
+                    }
+                });
+            }
+        }
+
+
+
+        // const user = await userModel.findAll({
+        //     attributes: { exclude: ['password', 'token'] },
+        //     order: [['id_register', 'DESC']]
+        // });
+
+        // return res.status(200).send({
+        //     status: 'success',
+        //     data: {
+        //         user
+        //     }
+        // });
 
     }
     catch (err) {
